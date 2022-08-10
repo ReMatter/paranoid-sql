3231341cbcdb44272f155bcd311b7ceee2044595
Nico Gallinal <nico@rematter.com>
2022-08-10 00:03:47 -0300

    remove traverse dependency

--- src/getParanoidSql.ts
+++ src/getParanoidSql.ts
@@ -1,5 +1,4 @@
-import { Parser } from 'node-sql-parser';
-import { forEach } from 'traverse';
+import { AST, From, Parser } from 'node-sql-parser';
 
 let parser: Parser;
 
@@ -25,51 +24,46 @@ const buildParanoidConditions = (tableAliases: string[]) =>
         } as any),
     );
 
-const getTableAliases = (descriptors: { table: string; as?: string }[]) =>
-  descriptors.map(({ table, as }) => as ?? table);
+const getTableAliases = (from: From[]) => from.map(({ table, as }) => as ?? table);
 
 export const getParanoidSql = (sql: string): string => {
   if (!parser) {
     parser = new Parser();
   }
-  const ast = parser.astify(sql);
-  forEach(ast, function (n) {
-    if (n.type === 'select') {
-      const tableAliases = getTableAliases(n.from);
-      const paranoidConditions = buildParanoidConditions(tableAliases);
-      if (n.where) {
-        n.where = {
-          type: 'binary_expr',
-          operator: 'AND',
-          left: n.where,
-          right: paranoidConditions,
-        };
-      } else {
-        n.where = paranoidConditions;
-      }
-      if (Array.isArray(n.columns)) {
-        n.columns.forEach(({ expr }: { expr: any }) => {
-          const ast = expr.ast;
-          if (ast?.type === 'select') {
-            const tableAliases = getTableAliases(ast.from);
-            const paranoidConditions = buildParanoidConditions(tableAliases);
-            if (ast.where) {
-              ast.where = {
-                type: 'binary_expr',
-                operator: 'AND',
-                left: ast.where,
-                right: paranoidConditions,
-              };
-            } else {
-              ast.where = paranoidConditions;
-            }
-          }
-        });
-      }
+  const ast = parser.astify(sql) as AST;
+  if (ast.type === 'select') {
+    const tableAliases = getTableAliases(ast.from as From[]);
+    const paranoidConditions = buildParanoidConditions(tableAliases);
+    if (ast.where) {
+      ast.where = {
+        type: 'binary_expr',
+        operator: 'AND',
+        left: ast.where,
+        right: paranoidConditions,
+      };
+    } else {
+      ast.where = paranoidConditions;
     }
-    // console.log(n);
-    this.update('');
-  });
+    if (Array.isArray(ast.columns)) {
+      ast.columns.forEach(({ expr }) => {
+        const ast: AST = expr.ast;
+        if (ast?.type === 'select') {
+          const tableAliases = getTableAliases(ast.from as From[]);
+          const paranoidConditions = buildParanoidConditions(tableAliases);
+          if (ast.where) {
+            ast.where = {
+              type: 'binary_expr',
+              operator: 'AND',
+              left: ast.where,
+              right: paranoidConditions,
+            };
+          } else {
+            ast.where = paranoidConditions;
+          }
+        }
+      });
+    }
+  }
 
   return parser.sqlify(ast);
 };
